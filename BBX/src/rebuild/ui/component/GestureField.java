2574b359fb7784a8502a783ad14bb473fd9f3bab
rcmaniac25 <devnull@localhost>
2014-02-22 22:03:58 -0500

    Prep for updating BB10 project (no longer UI specific)

--- BBX/src/rebuild/ui/component/GestureField.java
+++ BBX/src/rebuild/ui/component/GestureField.java
@@ -57,11 +57,15 @@ import net.rim.device.api.util.MathUtilities;
 //#endif
 
 //Based off: http://dwilson.org/blog/2009/4/19/implementing-pinch-zoom-on-the-iphone/
+//Rotation based off: http://en.wikipedia.org/wiki/Line-line_intersection
+
+//Special thanks to Sean McVeigh from RIM in getting me back on the right track with regards to rotation
 
 //TODO Update to support non-touchscreen interactions
 //TODO Update to support Pinch-Zoom Gesture (will require some work because it seems to be the cheap, scale-only type Pinch-zoom that all built-in smartphone systems do).
 //TODO Rotation component help: http://www.euclideanspace.com/maths/geometry/affine/aroundPoint/index.htm
 //TODO Very weird bug: If you do pinch-zoom where one finger is stationary and the other moves, you get different performance results. If you move one corner, it runs smooth. But move the other corner, it lags.
+//TODO Independent axis scaling
 //TODO Update for 7.1
 
 /**
@@ -129,6 +133,8 @@ public abstract class GestureField extends Field
 		private static final int TYPE_VALUE3 = (1 + 2) << 29;
 		private static final int TYPE_VALUE4 = (1 + 3) << 29;
 		private static final int TYPE_VALUE5 = (1 + 4) << 29;
+		private static final int TYPE_VALUE6 = (1 + 5) << 29;
+		private static final int TYPE_VALUE7 = (1 + 6) << 29;
 		private static final int TYPE_MASK = (1 + 6) << 29; //Limit should be 0xFFFF0000 or 65536 << 16
 		private static final int TYPE_MASK_INVERSE = ~TYPE_MASK;
 		
@@ -175,13 +181,21 @@ public abstract class GestureField extends Field
 		 */
 		public static final int TYPE_PINCH_ROTATE = TYPE_VALUE3 | EVENT_PINCH;
 		/**
+		 * Rotation center on the X coordinate in Fixed32 format.
+		 */
+		public static final int TYPE_PINCH_ROTATE_CENTER_X = TYPE_VALUE4 | EVENT_PINCH;
+		/**
+		 * Rotation center on the Y coordinate in Fixed32 format.
+		 */
+		public static final int TYPE_PINCH_ROTATE_CENTER_Y = TYPE_VALUE5 | EVENT_PINCH;
+		/**
 		 * Delta scale in the X plane in Fixed32 format.
 		 */
-		public static final int TYPE_PINCH_SCALE_X = TYPE_VALUE4 | EVENT_PINCH;
+		public static final int TYPE_PINCH_SCALE_X = TYPE_VALUE6 | EVENT_PINCH;
 		/**
 		 * Delta scale in the Y plane in Fixed32 format.
 		 */
-		public static final int TYPE_PINCH_SCALE_Y = TYPE_VALUE5 | EVENT_PINCH;
+		public static final int TYPE_PINCH_SCALE_Y = TYPE_VALUE7 | EVENT_PINCH;
 		
 		/**
 		 * Gesture direction that is equivalent to 180 degrees +/- 45 degrees relative to the device's current upward direction. Can be bitwise ORed with other 
@@ -220,8 +234,7 @@ public abstract class GestureField extends Field
 		public static final int SWIPE_WEST = 0x8;
 //#endif
 
-		
-		private int type, value1, value2, value3, value4, value5;
+		private int type, value1, value2, value3, value4, value5, value6, value7;
 		
 		private Gesture(int type, int[] values)
 		{
@@ -234,6 +247,8 @@ public abstract class GestureField extends Field
 					value3 = values[2];
 					value4 = values[3];
 					value5 = values[4];
+					value6 = values[5];
+					value7 = values[6];
 					break;
 			}
 		}
@@ -292,6 +307,10 @@ public abstract class GestureField extends Field
 						return this.value4;
 					case TYPE_VALUE5:
 						return this.value5;
+					case TYPE_VALUE6:
+						return this.value6;
+					case TYPE_VALUE7:
+						return this.value7;
 				}
 			}
 			throw new IllegalArgumentException();
@@ -347,9 +366,11 @@ public abstract class GestureField extends Field
 	/**
 	 * A rotation event occurred.
 	 * @param rad The delta rotation of the event in Fixed32 format.
+	 * @param cX The center of the rotation operation on the X coordinate in Fixed32 format.
+	 * @param cY The center of the rotation operation on the Y coordinate in Fixed32 format.
 	 * @return true if the gesture was handled, false if otherwise.
 	 */
-	protected boolean interactionRotate(int rad)
+	protected boolean interactionRotate(int rad, int cX, int cY)
 	{
 		//Doesn't do anything by default
 		return false;
@@ -418,24 +439,24 @@ public abstract class GestureField extends Field
 				{
 					argI = (int[])args;
 					if(argI[2] == 0 && //Rotation
-							argI[3] == Fixed32.ONE && argI[4] == Fixed32.ONE) //Original scale
+							argI[5] == Fixed32.ONE && argI[6] == Fixed32.ONE) //Original scale
 					{
 						//Only moving occurs so save some extra processing that the developer might have and memory
 						handled = interactionMove(argI[0], argI[1]);
 						break;
 					}
 					else if(argI[0] == 0 && argI[1] == 0 && //Translation
-							argI[3] == Fixed32.ONE && argI[4] == Fixed32.ONE) //Original scale
+							argI[5] == Fixed32.ONE && argI[6] == Fixed32.ONE) //Original scale
 					{
 						//Only rotation occurs so save some extra processing that the developer might have and memory
-						handled = interactionRotate(argI[2]);
+						handled = interactionRotate(argI[2], argI[3], argI[4]);
 						break;
 					}
 					else if(argI[0] == 0 && argI[1] == 0 && //Translation
 							argI[2] == 0) //Rotation
 					{
 						//Only scaling occurs so save some extra processing that the developer might have and memory
-						handled = interactionScale(argI[3], argI[4]);
+						handled = interactionScale(argI[5], argI[6]);
 						break;
 					}
 					else
@@ -563,67 +584,103 @@ public abstract class GestureField extends Field
 							//Onto second contact, now to do actual processing
 							contacts[1].x = Fixed32.toFP(message.getX(2));
 							contacts[1].y = Fixed32.toFP(message.getY(2));
-							motions = new int[13]; //transX, transY, rotate, scaleX, scaleY, ...temp variables
-							//motions[3] = motions[4] = Fixed32.ONE;
+							//motions = new int[15]; //transX, transY, rotate, rotateCenterX, rotateCenterY, scaleX, scaleY, ...temp variables
+							int transX; //0
+							int transY;
+							int rotate = 0;
+							int rCX = 0;
+							int rCY = 0;
+							int scaleX;
+							int scaleY; //4
+							
+							int temp1; //5
+							int temp2;
+							int temp3;
+							int temp4;
+							int temp5;
+							int temp6;
+							int temp7;
+							int temp8; //12
+							//scaleX = scaleY = Fixed32.ONE;
 							
 							//First the scale
 							//TODO: Add ability to do axis-independent scaling (maybe as an option so that when "pinching" occurs it can have either axis-independent or general scaling).
 							//Axis-independent is each axis is processed the same way but the other axis is not used in the process.
 							//sqrt((x2 - x1)^2 + (y2 - y1)^2)
-							motions[5] = start[1].x - start[0].x;
-							motions[6] = start[1].y - start[0].y;
-							motions[7] = contacts[1].x - contacts[0].x;
-							motions[8] = contacts[1].y - contacts[0].y;
+							temp1 = start[1].x - start[0].x;
+							temp2 = start[1].y - start[0].y;
+							temp3 = contacts[1].x - contacts[0].x;
+							temp4 = contacts[1].y - contacts[0].y;
 							//scale = newScale / prevScale, need to use doubles because Fixed32 is not "large" enough and the value "wraps"
-							motions[7] = (int)MathUtilities.round((
-									Math.sqrt(MathUtilities.pow(motions[7] * FP_2_DOUBLE, 2) + MathUtilities.pow(motions[8] * FP_2_DOUBLE, 2)) / 
-									Math.sqrt(MathUtilities.pow(motions[5] * FP_2_DOUBLE, 2) + MathUtilities.pow(motions[6] * FP_2_DOUBLE, 2))
+							temp3 = (int)MathUtilities.round((
+									Math.sqrt(MathUtilities.pow(temp3 * FP_2_DOUBLE, 2) + MathUtilities.pow(temp4 * FP_2_DOUBLE, 2)) / 
+									Math.sqrt(MathUtilities.pow(temp1 * FP_2_DOUBLE, 2) + MathUtilities.pow(temp2 * FP_2_DOUBLE, 2))
 											) * Fixed32.ONE
 										); //Currently there is no system to figure out X scale and Y scale, they are just there for future expansion.
 							
 							//The one problem with scaling a region is that the world origin is not the center of the object, so the object needs to be translated to 
 							//keep it in the same location
 							//-Figure out width/height of the selection area
-							motions[5] = Math.max(start[0].x, start[1].x) - (motions[9] = Math.min(start[0].x, start[1].x));
-							motions[6] = Math.max(start[0].y, start[1].y) - (motions[10] = Math.min(start[0].y, start[1].y));
+							temp1 = Math.max(start[0].x, start[1].x) - (temp5 = Math.min(start[0].x, start[1].x));
+							temp2 = Math.max(start[0].y, start[1].y) - (temp6 = Math.min(start[0].y, start[1].y));
 							//-Now get it's midpoint
-							XYRect extent = new XYRect(motions[9], motions[10], motions[5], motions[6]);
-							motions[9] = (extent.x + extent.X2()) >> 1; //In place of "value / 2"
-							motions[10] = (extent.y + extent.Y2()) >> 1; //Hoping that X2/Y2 are simply "return this.x/y + this.width/height;"
+							XYRect extent = new XYRect(temp5, temp6, temp1, temp2);
+							temp5 = (extent.x + extent.X2()) >> 1; //In place of "value / 2"
+							temp6 = (extent.y + extent.Y2()) >> 1; //Hoping that X2/Y2 are simply "return this.x/y + this.width/height;"
 							//-Figure out the difference in selection area size
-							motions[11] = Fixed32.mul(motions[5], motions[7]);
-							motions[12] = Fixed32.mul(motions[6], motions[7]);
-							motions[5] -= motions[11];
-							motions[6] -= motions[12];
+							temp7 = Fixed32.mul(temp1, temp3);
+							temp8 = Fixed32.mul(temp2, temp3);
+							temp1 -= temp7;
+							temp2 -= temp8;
 							//-The position of the midpoint of the touch as a percentage of the total distance
 							this.getExtent(extent);
-							motions[7] = motions[9] / extent.width;
-							motions[8] = motions[10] / extent.height;
+							temp3 = temp5 / extent.width;
+							temp4 = temp6 / extent.height;
 							//-Figure out the translation
-							motions[0] = Fixed32.mul(motions[5], motions[7]);
-							motions[1] = Fixed32.mul(motions[6], motions[8]);
+							transX = Fixed32.mul(temp1, temp3);
+							transY = Fixed32.mul(temp2, temp4);
 							
 							//Now apply the actual scale, since we have the "new width and height" we have an X and Y
-							motions[5] += motions[11]; //First recreate the original width/height
-							motions[6] += motions[12];
-							motions[3] = Fixed32.div(motions[11], motions[5]);
-							motions[4] = Fixed32.div(motions[12], motions[6]);
+							temp1 += temp7; //First recreate the original width/height
+							temp2 += temp8;
+							scaleX = Fixed32.div(temp7, temp1);
+							scaleY = Fixed32.div(temp8, temp2);
 							
 							//Now the rotation
-							//TODO
+							if((Fixed32.mul(start[0].x - start[1].x, contacts[0].y - contacts[1].y) - Fixed32.mul(start[0].y - start[1].y, contacts[0].x - contacts[1].x)) != 0) //We don't do anything with rotation if the lines are parallel
+							{
+								//Calculate rotation
+								rotate = Fixed32.atand2(contacts[1].y - contacts[0].y, contacts[1].x - contacts[0].x) - 
+										Fixed32.atand2(start[1].y - start[0].y, start[1].x - start[0].x);
+								
+								//Calculate rotation center
+								XYPoint center = determineRotationOrigin(start[0], start[1], contacts[0], contacts[1]);
+								rCX = center.x;
+								rCY = center.y;
+							}
 							
 							//Finally the translation
 							//TODO: Check adjustment from Scaling offset to make sure that it isn't moving too much, currently "overshooting" destination a bit
-							motions[5] = start[0].x + start[1].x;
-							motions[6] = start[0].y + start[1].y;
-							motions[7] = contacts[0].x + contacts[1].x;
-							motions[8] = contacts[0].y + contacts[1].y;
-							motions[5] >>= 1;
-							motions[6] >>= 1;
-							motions[7] >>= 1;
-							motions[8] >>= 1;
-							motions[0] -= motions[5] - motions[7]; //Add the translation so the "scale translation" doesn't get overridden
-							motions[1] -= motions[6] - motions[8];
+							temp1 = start[0].x + start[1].x;
+							temp2 = start[0].y + start[1].y;
+							temp3 = contacts[0].x + contacts[1].x;
+							temp4 = contacts[0].y + contacts[1].y;
+							temp1 >>= 1;
+							temp2 >>= 1;
+							temp3 >>= 1;
+							temp4 >>= 1;
+							transX -= temp1 - temp3; //Add the translation so the "scale translation" doesn't get overridden
+							transY -= temp2 - temp4;
+							
+							//XXX
+							motions = new int[7];
+							motions[0] = transX;
+							motions[1] = transY;
+							motions[2] = rotate;
+							motions[3] = rCX;
+							motions[4] = rCY;
+							motions[5] = scaleX;
+							motions[6] = scaleY;
 							
 							//Reset starting point and invoke callbacks
 							start[0].x = contacts[0].x;
@@ -668,47 +725,31 @@ public abstract class GestureField extends Field
 		}
 	}
 	
-	private static XYPoint determineRotationOrigin(int angle, XYPoint preRot, XYPoint postRot)
+	private static XYPoint determineRotationOrigin(XYPoint L11, XYPoint L12, XYPoint L21, XYPoint L22)
 	{
-		//Code doesn't seem accurate enough. When testing with preRot = {50,50}, angle = 45 degrees, and rotation center = {10,11} it returns {7.77..., 11.797...}
+		//Uses the less efficient Double type so overflow/underflow don't occur
 		
-		//First create a simple rotation matrix (no rotation center)
-		int[] mat = new int[9];
-		GestureField.matrixSetRotate(mat, 0, angle);
+		double x1 = L11.x * FP_2_DOUBLE;
+		double x2 = L12.x * FP_2_DOUBLE;
+		double x3 = L21.x * FP_2_DOUBLE;
+		double x4 = L22.x * FP_2_DOUBLE;
+		double y1 = L11.y * FP_2_DOUBLE;
+		double y2 = L12.y * FP_2_DOUBLE;
+		double y3 = L21.y * FP_2_DOUBLE;
+		double y4 = L22.y * FP_2_DOUBLE;
 		
-		//Next figure out translation of matrix
-		mat[2] = (postRot.x - Fixed32.mul(preRot.x, mat[0])) - Fixed32.mul(preRot.x, mat[1]);
-		mat[5] = (postRot.y - Fixed32.mul(preRot.y, mat[4])) - Fixed32.mul(preRot.y, mat[3]);
-		/* Original C# source
-		 * mat.OffsetX = (postRot.X - (preRot.X * mat.M11)) - (preRot.X * mat.M21);
-		 * mat.OffsetY = (postRot.Y - (preRot.Y * mat.M22)) - (preRot.Y * mat.M12);
-		 */
+		double x1x2 = x1 - x2;
+		double x3x4 = x3 - x4;
+		double y1y2 = y1 - y2;
+		double y3y4 = y3 - y4;
 		
-		/* Finally figure out the rotation origin
-		 * Uses Cramer's rule and the coefficient determinant to figure out rotation origin
-		 * a*x + b*y = c
-		 * d*x + e*y = f
-		 * 
-		 * x = (c*e - f*b) / D
-		 * y = (a*f - d*c) / D
-		 * D = a*e - b*d
-		 * 
-		 * Uses the less efficient Double type so overflow/underflow don't occur
-		 */
-		double aNe = (Fixed32.ONE - mat[0]) * FP_2_DOUBLE;
-		double mat1 = mat[1] * FP_2_DOUBLE;
-		double mat2 = mat[2] * FP_2_DOUBLE;
-		double mat3 = mat[3] * FP_2_DOUBLE;
-		double mat5 = mat[5] * FP_2_DOUBLE;
-		double D = MathUtilities.pow(aNe, 2) - (mat3 * mat1);
-		return new XYPoint((int)MathUtilities.round((((mat2 * aNe) - (mat5 * mat3) / D)) * Fixed32.ONE), 
-				(int)MathUtilities.round((((aNe * mat5) - (mat1 * mat2) / D)) * Fixed32.ONE));
-		/* Original C# source
-		 * double inverseCos = 1.0 - mat.M11;
-		 * double D = (inverseCos * inverseCos) - (mat.M12 * mat.M21);
-		 * return new Point(((mat.OffsetX * inverseCos) - (mat.OffsetY * mat.M12)) / D,
-		 * 		((inverseCos * mat.OffsetY) - (mat.M21 * mat.OffsetX)) / D);
-		 */
+		double xyyx12 = (x1 * y2) - (y1 * x2);
+		double xyyx34 = (x3 * y4) - (y3 * x4);
+		
+		double denom = (x1x2 * y3y4) - (y1y2 * x3x4);
+		
+		return new XYPoint((int)MathUtilities.round(((xyyx12 * x3x4) - (x1x2 * xyyx34)) / denom) * Fixed32.ONE,
+				(int)MathUtilities.round(((xyyx12 * y3y4) - (y1y2 * xyyx34)) / denom) * Fixed32.ONE);
 	}
 	
 	/**
